340892338a875c4e14a5495e937ec62556c5b8bb
zhanghongwei06 <zhanghongwei06@58.com>
2022-07-06 10:05:36 +0800

    修改部分bug

--- agent_service/src/main/java/com/zhw/javaagent/AgentService.java
+++ agent_service/src/main/java/com/zhw/javaagent/AgentService.java
@@ -6,7 +6,6 @@ import org.apache.http.client.methods.HttpGet;
 public class AgentService {
     // agent地址:D:\javaWorkspace\gitHub\javaAgent\agent_client\target\agent_client-1.0.1.jar
     public static void main(String[] args) {
-        System.out.println(ClassLoader.getSystemResource(""));
         System.out.println("AgentService.main()方法被执行");
         HttpGet httpGet = new HttpGet();
         System.out.println(httpGet.getMethod());
